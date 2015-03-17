dcbfbe73cde79de71c53712d9cf8a4115f44ba2e
Wyatt Childers <wchilders@nearce.com>
2015-03-16 21:46:02 -0400

    Documented the main

--- main.cpp
+++ main.cpp
@@ -33,12 +33,14 @@ template <typename T>
 
 int main(int argc, char* argv[]) {
   if (argc == 4) {
+    // Trigger a simulation based on arguments
     double chance = std::atof(argv[1]);
     unsigned int runs = std::atoi(argv[2]);
     unsigned int boardSize = std::atoi(argv[3]);
 
     simulate(runs, chance, boardSize);
   } else if (argc == 2) {
+    // Count the clusters in the specified file
     readFile(argv[1]);
   } else {
     throw std::runtime_error("Illegal number of arguments!");
@@ -46,12 +48,39 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
+/** Generate grids, and test their percolation.
+
+    Generates the specified amount of mazes, with a specified size,
+    and specified percolation chance for each grid entry.
+
+    After running the simulation, the funtion prints the overall
+    percolation rate -- the number of percolating mazes / the number
+    of generated mazes -- in both ratio and percent format.
+
+    @param runs
+    The number of mazes to generate and test.
+
+    @param chance
+    The per-grid entry chance of percolation.
+
+    @param boardSize
+    The size of the maze board to generate, this is used for
+    both width, and height.
+ */
 void simulate(unsigned int runs, double chance, unsigned int boardSize) {
+  // Initialize a maze fatory with the std::minstd_rand random engine
+  // seeded by the number 8.
   MazeFactory factory(std::minstd_rand(8));
+
+  // Count the number of percolating maze boards
   unsigned int percolations = 0;
   for (unsigned int i = 0; i < runs; ++i) {
+    // Generate a maze board, using the maze factory
     Grid<bool> grid = factory.generate(chance, boardSize, boardSize);
+
+    //Find the clusters in the generated maze board's grid
     auto res = findCluster(grid);
+    // Check to see if the maze percolates
     if (percolates(grid, res)) {
       ++percolations;
     }
@@ -61,11 +90,28 @@ void simulate(unsigned int runs, double chance, unsigned int boardSize) {
             << std::endl;
 }
 
+/** Read a maze file and count the clusters which percolate.
+
+    Takes the given maze file, loads in into memory,
+    and counts the number of clusters it contains which
+    percolate. The function then prints this number.
+
+    @param fileName
+    The name of the file to load.
+ */
 void readFile(std::string fileName) {
+  // Load the maze file into a Grid<bool>
   auto grid = loadMazeFromFile(fileName);
+
+  // Find clusters in the maze board's grid
   auto res = findCluster(grid);
+
+  // Count the number of root nodes, checking the grid
+  // to see if the cluster percolates, or is solid
   unsigned int clusters = 0;
   for (unsigned int i = 0; i < res.size(); ++ i) {
+    // if (!res[i].hasVal()) - the node is a root node
+    // if (grid[i]) - the node percolates
     if (!res[i].hasVal() && grid[i]) {
       ++clusters;
     }
@@ -73,6 +119,14 @@ void readFile(std::string fileName) {
   std::cout << "Clusters: " << clusters << std::endl;
 }
 
+/** Prints the given Grid<T>.
+
+    Takes the given grid, and prints the values row by row
+    in grid format, columns seperated by spaces.
+
+    @param grid
+    The grid to print.
+ */
 template <typename T>
   void printBoard(Grid<T> grid) {
     for (unsigned int h = 0; h < grid.getHeight(); ++h) {
